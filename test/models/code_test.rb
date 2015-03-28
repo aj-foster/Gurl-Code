2b75f2e92f2fd33b7ca4cc2f1ebe0f771496c0f9
AJ Foster <web@aj-foster.com>
2015-03-27 23:36:20 -0400

    Add first code test for title presence validation.
    Also, remove user fixtures as they were causing tests to fail due to non-
    unique user emails.

--- test/models/code_test.rb
+++ test/models/code_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class CodeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save code without a title" do
+    code = Code.new
+    assert_not code.save, "Saved a code without a title"
+  end
 end
