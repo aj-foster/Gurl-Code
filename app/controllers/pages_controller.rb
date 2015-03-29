67a7a8519d61a33c4fb8cde9f38d6bbd3dc85e27
AJ Foster <web@aj-foster.com>
2015-03-29 02:23:17 -0400

    Add preliminary home view.
    Still needs voting overlays, login, responsiveness, and a footer. But, it is a
    good start.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,10 @@ class PagesController < ApplicationController
   def home
     @code = Code.where(status: "Featured").first
     @submissions = @code.submissions.limit(2) unless @code.nil?
+
+    if @code.nil? || @submissions.size != 2
+      render :gallery
+    end
   end
 
   def coming_soon
