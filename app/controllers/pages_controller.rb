cae555deade2ac41b80f4d996b0e079c8110bac8
AJ Foster <web@aj-foster.com>
2015-03-29 01:01:08 -0400

    Add about and gallery page stubs.
    Also, the header links will stay "active" when you visit these pages.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,16 @@
 class PagesController < ApplicationController
 
+  def about
+  end
+
   def home
     @code = Code.where(status: "Featured").first
     @submissions = @code.submissions.limit(2) unless @code.nil?
   end
 
   def coming_soon
+  end
+
+  def gallery
   end
 end
